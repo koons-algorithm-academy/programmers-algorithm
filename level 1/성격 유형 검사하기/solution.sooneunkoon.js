0eb54a94ba4c51c3f8940b215bf70f0e0e594f18
hwangsooneun <catalyst8849@gmail.com>
2025-04-23 16:09:40 +0900

    Sol: Level 1 성격 유형 검사하기 - 순은 풀이

--- level 1/성격 유형 검사하기/solution.sooneunkoon.js
+++ level 1/성격 유형 검사하기/solution.sooneunkoon.js
@@ -1,4 +1,4 @@
-function solution(survey, choices) {
+function FailSolution(survey, choices) {
     var answer = '';
     const pointMap = {}
     const indicators = [['R', 'T'], ['C', 'F'], ['J', 'M'], ['A', 'N']]
@@ -28,4 +28,35 @@ function solution(survey, choices) {
 /**
  * 95 / 100
  * 엣지 케이스를 아직 찾지 못함
+ */
+
+function solution(survey, choices) {
+    var answer = '';
+    const pointMap = {}
+    const indicators = [['R', 'T'], ['C', 'F'], ['J', 'M'], ['A', 'N']]
+    survey.forEach((el, idx) => {
+        const type1 = el[0]
+        const type2 = el[1]
+        const select = choices[idx]
+        const selectType = select < 4 ? type1 : type2
+        const point = Math.abs(select - 4)
+        if (point) {
+            pointMap[selectType] = (pointMap[selectType] ?? 0) + point
+        }
+        
+    })
+    indicators.forEach(el => {
+        const type1Point = pointMap[el[0]] ?? 0
+        const type2Point = pointMap[el[1]] ?? 0
+        if (type1Point >= type2Point) {
+            answer += el[0]
+        } else {
+            answer += el[1]
+        }
+    })
+    return answer;
+}
+
+/** 
+ * 포인트를 더하는 부분에서 최초 포인트의 경우 point * 2가 더해지고 있었음.
  */
