111d3e536b0263398510577367f3d0d8b7247451
hyoni0817 <hyoni0817@naver.com>
2025-04-26 03:25:28 +0900

    Sol: Level 1 콜라 문제 - 나현 풀이

--- level 1/콜라 문제/solution.nahyunkoon.js
+++ level 1/콜라 문제/solution.nahyunkoon.js
@@ -4,14 +4,17 @@ function solution(a, b, n) {
     let remainBottle = 0;
     
     while (emptyBottle >= b) {
-        const tempBottle = emptyBottle;
+        const copyEmptyBottle = emptyBottle;        
         emptyBottle = Math.floor((emptyBottle + remainBottle) / a) * b;
-        remainBottle = tempBottle % a;
+        remainBottle = (copyEmptyBottle + remainBottle) % a;
         
-        console.log(tempBottle, emptyBottle, remainBottle)
-        
-        answer += emptyBottle;     
+        answer += emptyBottle;
     }
     
     return answer;
 }
+
+/**
+ * 빈 병을 콜라로 바꾸는 과정에서 생기는 남은 빈병들을 계속 포함시켜서 계산하는 것이 핵심
+ * 남은 콜라 빈 병(remainBottle)을 다음 계산 식에서 포함시켜서 계산되게 함.
+ */
