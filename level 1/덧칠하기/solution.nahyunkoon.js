f53b01fbbefb43916ab88ceddd4290b114d20b41
hyoni0817 <hyoni0817@naver.com>
2025-06-15 01:01:43 +0900

    Update: Level 1 덧칠하기  - 나현 풀이 방법 주석 수정

--- level 1/덧칠하기/solution.nahyunkoon.js
+++ level 1/덧칠하기/solution.nahyunkoon.js
@@ -21,6 +21,6 @@ function solution(n, m, section) {
  * 1. 마지막으로 덧칠한 위치를 lastPaintedSection 변수에 저장
  * 2. section 배열 길이 만큼 순회
  *    1) section[i]가 lastPaintedSection보다 작거나 같으면 넘어감
- *    2) 아니면 lastPaintedSection를 칠할 수 있는 최대 위치인 section[i] + m - 1로 업데이트하고, answer를 1 증가
+ *    2) section[i]가 lastPaintedSection보다 크다면, section[i] 부터 롤러로 다시 밀면서 덧칠한다고 생각하고 lastPaintedSection를 칠할 수 있는 최대 위치인 section[i] + m - 1로 업데이트 한 뒤 answer를 1 증가
  * 3. answer 반환
  */
